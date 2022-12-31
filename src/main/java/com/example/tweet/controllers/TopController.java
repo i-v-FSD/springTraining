18c7429a65c805096b4e3c04763c4549c18847ac
iVinci-AkitoYOSHIMURA <akito.yoshimura@i-vinci.co.jp>
2022-12-31 18:39:10 +0900

    [add]serviceメソッド呼び出し
    ・ControllerはModelを用いて値のやり取りをする用に変更

--- src/main/java/com/example/tweet/controllers/TopController.java
+++ src/main/java/com/example/tweet/controllers/TopController.java
@@ -2,11 +2,21 @@ package com.example.tweet.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.example.tweet.services.TweetService;
 
 @Controller
 public class TopController {
     @RequestMapping("/")
-    public String topTweetPageString() {
-        return "/top.html";
+    public ModelAndView topTweetPageString() {
+        // 遷移先画面を設定
+        ModelAndView model = new ModelAndView("/top.html");
+
+        TweetService tweetService = new TweetService();
+        String tweetContent = tweetService.fetchTweetList();
+        // 画面へ値を渡すため、ModelAndViewに値を詰める
+        model.addObject("tweet", tweetContent);
+        return model;
     }
 }
---
+++ src/main/java/com/example/tweet/services/TweetService.java
@@ -0,0 +1,8 @@
+package com.example.tweet.services;
+
+public class TweetService {
+    // 動作確認として文字列を返すだけ
+    public String fetchTweetList() {
+        return "fetchTweetList";
+    }
+}
