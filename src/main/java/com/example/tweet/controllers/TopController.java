c3f7996331707d2399b91c9d4d21609c0f20356f
iVinci-AkitoYOSHIMURA <akito.yoshimura@i-vinci.co.jp>
2023-04-29 23:24:49 +0900

    ボタン押下時の遷移方法をリダイレクトに変更

--- src/main/java/com/example/tweet/controllers/TopController.java
+++ src/main/java/com/example/tweet/controllers/TopController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.example.tweet.entities.Tweet;
 import com.example.tweet.services.TweetService;
@@ -31,7 +32,7 @@ public class TopController {
     }
 
     @PostMapping("/insert")
-    public ModelAndView InsertTweet(HttpServletRequest request) {
+    public String InsertTweet(HttpServletRequest request, RedirectAttributes redirectAttributes) {
         int userId = Integer.parseInt(request.getParameter("user_id"));
         String content = (String) request.getParameter("content");
 
@@ -39,24 +40,20 @@ public class TopController {
         System.out.println(resultNum + "件のTweetを追加");
         List<Tweet> tweetContent = tweetService.fetchTweetList();
 
-        // 遷移先画面を設定
-        ModelAndView model = new ModelAndView("/top.html");
-        model.addObject("tweetList", tweetContent);
-        return model;
+        redirectAttributes.addFlashAttribute("tweetList", tweetContent);
+        return "redirect:/";
     }
 
     @PostMapping("/delete")
-    public ModelAndView deleteTweet(HttpServletRequest request) {
+    public String deleteTweet(HttpServletRequest request, RedirectAttributes redirectAttributes) {
         int id = Integer.parseInt(request.getParameter("id"));
         // 削除ボタン押下されたTweetのIDを利用して削除
         int resultNum = tweetService.deleteTweet(id);
         System.out.println(resultNum + "件のTweetを削除");
         List<Tweet> tweetContent = tweetService.fetchTweetList();
 
-        // 遷移先画面を設定
-        ModelAndView model = new ModelAndView("/top.html");
-        model.addObject("tweetList", tweetContent);
-        return model;
+        redirectAttributes.addFlashAttribute("tweetList", tweetContent);
+        return "redirect:/";
     }
 
     @GetMapping("/edit{id}")
@@ -66,22 +63,19 @@ public class TopController {
         // 編集ボタン押下されたTweetのIDを利用して削除
         Tweet editTargetTweet = tweetService.selectTweetById(tweetId);
 
-        // 遷移先画面を設定
         model.addObject("tweet", editTargetTweet);
         return model;
     }
 
     @PostMapping("/edit")
-    public ModelAndView updateProcess(HttpServletRequest request) {
+    public String updateProcess(HttpServletRequest request, RedirectAttributes redirectAttributes) {
         int tweetId = Integer.parseInt(request.getParameter("id"));
         String content = (String) request.getParameter("content");
         String updatedMessage = tweetService.updateTweetById(tweetId, content);
         List<Tweet> tweetContent = tweetService.fetchTweetList();
 
-        // 遷移先画面を設定
-        ModelAndView model = new ModelAndView("/top.html");
-        model.addObject("updatedMessage", updatedMessage);
-        model.addObject("tweetList", tweetContent);
-        return model;
+        redirectAttributes.addFlashAttribute("message", updatedMessage);
+        redirectAttributes.addFlashAttribute("tweetList", tweetContent);
+        return "redirect:/";
     }
 }
