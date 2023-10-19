5763b6c35ccadd5d4d42804aac413d9eb4beedaf
iVinci-AkitoYOSHIMURA <akito.yoshimura@i-vinci.co.jp>
2023-10-20 00:33:44 +0900

    MappingをGetに変更

--- src/main/java/com/example/tweet/controllers/TopController.java
+++ src/main/java/com/example/tweet/controllers/TopController.java
@@ -8,7 +8,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.example.tweet.entities.Tweet;
@@ -24,7 +23,7 @@ public class TopController {
     @Autowired
     private TweetService tweetService;
 
-    @RequestMapping("/")
+    @GetMapping("/")
     public String topTweetPageString(Model model) {
         try {
             List<Tweet> tweetContent = tweetService.fetchTweetList();
