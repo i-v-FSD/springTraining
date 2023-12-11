2764184d379ff8277e9a40ab3c938aa5bf17af97
iVinci-KenichiRan <kenichi.ran@i-vinci.co.jp>
2023-12-12 08:52:36 +0900

    [modify]初期データ定義修正

--- mysql/init/insert_data.sql
+++ mysql/init/insert_data.sql
@@ -1,19 +1,19 @@
 INSERT INTO
     `user` (id, name, email, password)
 VALUES
-    ("1", '山田','hoge@fuga.com', 'pass123');
+    ("1", '山田太郎','taro@example.com', 'pass123');
 
 INSERT INTO
     `user` (id, name, email, password)
 VALUES
-    ("2", '佐藤','fuga@fuga.com', 'pass123');
+    ("2", '佐藤和樹','kazuki@example.com', 'pass123');
 
 INSERT INTO
     `user` (id, name, email, password)
 VALUES
-    ("3", '合田','piyo@fuga.com', 'pass123');
+    ("3", '合田健司','kenji@example.com', 'pass123');
 
 INSERT INTO
-    `user` (id, name, email, password)
+    `tweet` (id, user_id, content)
 VALUES
-    ("4", '鈴木',"suzuki@fuga.com", 'Hello Tweet');
+    ("1", "1", 'Hello');
