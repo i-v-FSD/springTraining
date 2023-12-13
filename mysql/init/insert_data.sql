80058bc6d9d37701737772c9aa3e6fcd6d2161e2
iVinci-KenichiRan <kenichi.ran@i-vinci.co.jp>
2023-12-13 11:50:53 +0900

    [modify]クォーテーション統一、スペース調整

--- mysql/init/insert_data.sql
+++ mysql/init/insert_data.sql
@@ -1,19 +1,19 @@
 INSERT INTO
     `user` (id, name, email, password)
 VALUES
-    ("1", '山田太郎','taro@example.com', 'pass123');
+    ('1', '山田太郎', 'taro@example.com', 'pass123');
 
 INSERT INTO
     `user` (id, name, email, password)
 VALUES
-    ("2", '佐藤和樹','kazuki@example.com', 'pass123');
+    ('2', '佐藤和樹', 'kazuki@example.com', 'pass123');
 
 INSERT INTO
     `user` (id, name, email, password)
 VALUES
-    ("3", '合田健司','kenji@example.com', 'pass123');
+    ('3', '合田健司', 'kenji@example.com', 'pass123');
 
 INSERT INTO
     `tweet` (id, user_id, content)
 VALUES
-    ("1", "1", 'Hello');
+    ('1', '1', 'Hello');
